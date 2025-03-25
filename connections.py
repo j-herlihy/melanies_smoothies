d7580c1f86b54ba1745eca5d0e2778babceb28fc
j-herlihy <jilldherlihy@gmail.com>
2025-03-25 00:22:01 -0400

    Add files via upload

---
+++ connections.py
@@ -0,0 +1,8 @@
+[connections.snowflake]
+account = "NBRNPAD-EAB38597"
+user = "JHERLIHY"
+passwork = "CCsnowflake123"
+role = "SYSADMIN"
+warehouse = "COMPUTE_WH"
+database = "SMOOTHIES"
+schema = "PUBLIC"
