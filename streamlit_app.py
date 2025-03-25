35ddb98416a13231911c2018a258ed35df7a7432
j-herlihy <jilldherlihy@gmail.com>
2025-03-25 01:21:23 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import streamlit as st
 from snowflake.snowpark.functions import col
 cnx=st.connection("snowflake")
 import requests
+import pandas
 
 helpful_links = [
     "https://docs.streamlit.io",
