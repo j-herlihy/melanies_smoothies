c80fadff306cf5c93282a6b5c04104dc777f0e67
j-herlihy <jilldherlihy@gmail.com>
2025-03-25 00:23:25 -0400

    Update connections.py

--- connections.py
+++ connections.py
@@ -1,7 +1,7 @@
 [connections.snowflake]
 account = "NBRNPAD-EAB38597"
 user = "JHERLIHY"
-passwork = "CCsnowflake123"
+password = "CCsnowflake123"
 role = "SYSADMIN"
 warehouse = "COMPUTE_WH"
 database = "SMOOTHIES"
